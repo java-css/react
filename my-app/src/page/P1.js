a0687e39ed7c679e93e74bee88ab32bd0b64f67c
Jason <1053384819@qq.com>
2018-08-31 16:13:25 +0800

    jason

--- my-app/src/page/P1.js
+++ my-app/src/page/P1.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
-import QP from "../component/QueryPannel";
-import TP from "../component/TablePannel";
+import QP from "../component/QueryPannel"
+import TP from "../component/TablePannel"
+import ee from "../store/1"
 
 class P1 extends Component {
   constructor(props) {
-    super(props);
-    this.state = { data: [] };
+    super(props)
+    this.state = { data: [] }
+
+    ee.on("message", text => {
+      this.setState({ data: text })
+    })
   }
-  handleChange(e) {
-    console.log(e);
-    this.setState({ data: e });
-  }
+
+  // handleChange(e) {
+  //   console.log(e)
+  //   this.setState({ data: e })
+  // }
   render() {
     return (
       <div style={style.bg}>
-        <QP onHandleChange={this.handleChange.bind(this)} />
+        {/* <QP onHandleChange={this.handleChange.bind(this)} /> */}
+        <QP />
         <TP data={this.state.data} />
       </div>
-    );
+    )
   }
 }
 
 const style = {
   bg: {
-    backgroundColor: "red"
-  }
-};
+    backgroundColor: "red",
+  },
+}
 
-export default P1;
+export default P1
